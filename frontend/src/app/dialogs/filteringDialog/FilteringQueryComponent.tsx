9646d711dca585b25d6c455aaf2ed5413b9fb7fd
weezard <veelmoth@gmail.com>
2025-02-25 08:21:28 +0100

    Sort options
    Sort the options by the local compare. This case, the options under the input field will be displayed in a correct order

--- frontend/src/app/dialogs/filteringDialog/FilteringQueryComponent.tsx
+++ frontend/src/app/dialogs/filteringDialog/FilteringQueryComponent.tsx
@@ -31,7 +31,7 @@ type Props = {
 const FilteringQueryComponent = React.memo(function FilteringQueryComponent({ id }: Props) {
   console.log("[FilteringQueryComponent]: rendered");
 
-  const queryByOptions = Object.values(FilterDialogFilters).sort();
+  const queryByOptions = Object.values(FilterDialogFilters).sort((a, b) => a.localeCompare(b));
 
   const dispatch = useDispatch();
 
