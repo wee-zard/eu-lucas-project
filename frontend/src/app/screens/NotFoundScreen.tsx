4871ca9a51e7da8bbcc8cdf2f473fafef8dd3852
weezard <veelmoth@gmail.com>
2025-04-12 19:30:21 +0200

    Fixing "Not Found" title
    - The "Not Found" title was not displayed properly, due to the missing of the dot.

--- frontend/src/app/screens/NotFoundScreen.tsx
+++ frontend/src/app/screens/NotFoundScreen.tsx
@@ -1,14 +1,8 @@
 import i18n from "@i18n/i18nHandler";
 
 const NotFoundScreen = () => {
-  /*
-  useEffect(() => {
-    redirectToUrl(ScreenUrls.LoginScreenPath);
-  }, []);
-  */
-
-  /** TODO: Impement a default 404 Not Found page. */
-  return <div>{i18n.t("screens.not-found-page-not-found")}</div>;
+  /** TODO: Implement a default 404 Not Found page. */
+  return <div>{i18n.t("screens.not-found.page-not-found")}</div>;
 };
 
 export default NotFoundScreen;
