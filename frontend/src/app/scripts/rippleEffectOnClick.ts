db744aa71a9ad577954eb7dc4a34d1a65c5da48d
weezard <veelmoth@gmail.com>
2025-04-12 19:29:27 +0200

    Updating the ripple effect.
    .

--- frontend/src/app/scripts/rippleEffectOnClick.ts
+++ frontend/src/app/scripts/rippleEffectOnClick.ts
@@ -7,30 +7,33 @@
  */
 export const handleClickOnGlobalRippleEffect = (event: React.MouseEvent, id: string) => {
   const imageCard = document.getElementById(id);
-  if (imageCard) {
-    // Create span element
-    let ripple = document.createElement("span");
 
-    // Add ripple class to span
-    ripple.classList.add("ripple");
-
-    // Add span to the button
-    imageCard.appendChild(ripple);
-
-    // Move the span to a default position
-    ripple.style.left = `0px`;
-    ripple.style.top = `0px`;
-
-    // Get the position of the span.
-    var rect = ripple.getBoundingClientRect();
-
-    // Position the span element
-    ripple.style.left = `${event.clientX - rect.x}px`;
-    ripple.style.top = `${event.clientY - rect.y}px`;
-
-    // Remove span after 0.75sec
-    setTimeout(() => {
-      ripple.remove();
-    }, 750);
+  if (!imageCard) {
+    return;
   }
+
+  // Create span element
+  let ripple = document.createElement("span");
+
+  // Add ripple class to span
+  ripple.classList.add("ripple");
+
+  // Add span to the button
+  imageCard.appendChild(ripple);
+
+  // Move the span to a default position
+  ripple.style.left = `0px`;
+  ripple.style.top = `0px`;
+
+  // Get the position of the span.
+  var rect = ripple.getBoundingClientRect();
+
+  // Position the span element
+  ripple.style.left = `${event.clientX - rect.x}px`;
+  ripple.style.top = `${event.clientY - rect.y}px`;
+
+  // Remove span after 0.75sec
+  setTimeout(() => {
+    ripple.remove();
+  }, 750);
 };
