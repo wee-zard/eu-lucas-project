6c094fdcfbd01de14b5b0c40b9467d770a3274ec
weezard <veelmoth@gmail.com>
2025-06-12 20:34:24 +0200

    Update DateHelper
    - Removing the Z character from the custom date time format.

--- frontend/src/app/helper/dateHelper.ts
+++ frontend/src/app/helper/dateHelper.ts
@@ -1,6 +1,6 @@
 abstract class DateHelper {
   public static convertISOStringToDateTimeFormat = (isoString: string) => {
-    return isoString.split(".")[0].replaceAll("-", ".").replace("T", ". ");
+    return isoString.split(".")[0].replaceAll("-", ".").replace("T", ". ").replace("Z", "");
   };
 }
 
