1ac3c670b4521047c33a284b9f969ed93377c254
weezard <veelmoth@gmail.com>
2025-06-18 11:44:43 +0200

    Creating folder tables
    - Writing a new sql file for creating the folder, the shared folder, folder content, query builder and query element tables for the purpose of saving the user created folders, the images inside those folders, and the queries that helped fetching the images.

---
+++ spring-boot-backend/src/main/resources/db/migration/lucasdb/V202506151509__create_table_folder.sql
@@ -0,0 +1,48 @@
+create table tb_folder (
+    id bigint AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(500) NULL,
+    owner_user_id bigint NOT NULL,
+    created_at Timestamp,
+    updated_at Timestamp,
+    CONSTRAINT tb_folder_fk_1 foreign key(owner_user_id) references tb_user_root(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+create table tb_share_folder (
+    folder_id bigint,
+    shared_with_user_id bigint,
+    is_editable TINYINT(1) NOT NULL,
+    created_at Timestamp,
+    updated_at Timestamp,
+    PRIMARY KEY(folder_id, shared_with_user_id),
+    CONSTRAINT tb_share_album_fk_1 foreign key(shared_with_user_id) references tb_user_root(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT tb_share_album_fk_2 foreign key(folder_id) references tb_folder(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+create table tb_query_builder (
+    id bigint PRIMARY KEY,
+    parent_id bigint NULL,
+    element_relationship TINYINT(1),
+    type TINYINT(1) NOT NULL,
+    CONSTRAINT tb_query_builder_fk_1 foreign key(parent_id) references tb_query_builder(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+create table tb_query_element (
+    id bigint PRIMARY KEY,
+    query_builder_id bigint,
+    filter_tab int(2) NOT NULL,
+    operator int(2) NOT NULL,
+    select_input varchar(100) NULL,
+    text_field varchar(100) NULL,
+    CONSTRAINT tb_query_element_fk_1 foreign key(query_builder_id) references tb_query_builder(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+create table tb_folder_content (
+    folder_id bigint,
+    image_id bigint,
+    query_builder_id bigint NOT NULL,
+    PRIMARY KEY(folder_id, image_id),
+    CONSTRAINT tb_folder_content_fk_1 foreign key(folder_id) references tb_folder(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT tb_folder_content_fk_2 foreign key(image_id) references tb_image(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT tb_folder_content_fk_3 foreign key(query_builder_id) references tb_query_builder(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
