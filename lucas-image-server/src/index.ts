ac14ad4ef5180480204cb5499b739299751ef52d
weezard <veelmoth@gmail.com>
2025-10-01 09:27:07 +0200

    upscaling the limit of the bodyParser
    upscaling the limit of the bodyParser from 50mb to 150mb

--- lucas-image-server/src/index.ts
+++ lucas-image-server/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(configureCorsMiddleware());
 
 // Add bodyParser
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.json({ limit: '150mb' }));
 
 // Add routes
 app.use('/api', configureRoutesMiddleware());
