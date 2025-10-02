8966e2c8cf8cddc62914de881dfb4552469c8417
weezard <veelmoth@gmail.com>
2025-10-02 17:21:35 +0200

    Update Application.java
    - Allowing the use of Scheduled annotation.

--- spring-boot-backend/src/main/java/com/lucas/spring/Application.java
+++ spring-boot-backend/src/main/java/com/lucas/spring/Application.java
@@ -3,12 +3,14 @@ package com.lucas.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * The entry class of our application.
  */
 @SpringBootApplication
 @EnableCaching
+@EnableScheduling
 public class Application {
   /**
     * The entry point of our application.
